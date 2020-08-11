83ff18cd42af17938af28a9e0361d62e340d5ea7
Amit K Sharma <340amit@gmail.com>
2020-08-11 23:26:18 +0200

    integration Test Cases

---
+++ src/test/java/com/dexter/kabutar/PostgressTestContainer.java
@@ -0,0 +1,34 @@
+package com.dexter.kabutar;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class PostgressTestContainer extends PostgreSQLContainer<PostgressTestContainer> {
+
+    private static final String IMAGE_VERSION = "postgres:11.1";
+
+    private static PostgressTestContainer postgressTestContainer;
+
+    private PostgressTestContainer(){
+        super(IMAGE_VERSION);
+    }
+
+    public static PostgressTestContainer getInstance(){
+        if(postgressTestContainer == null){
+            postgressTestContainer = new PostgressTestContainer();
+        }
+        return postgressTestContainer;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", postgressTestContainer.getJdbcUrl());
+        System.setProperty("DB_USERNAME", postgressTestContainer.getUsername());
+        System.setProperty("DB_PASSWORD", postgressTestContainer.getPassword());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+}
---
+++ src/test/java/com/dexter/kabutar/UserIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.dexter.kabutar;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserIntegrationTest {
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = PostgressTestContainer.getInstance();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void successfullyAddUser() throws Exception {
+        String request = "{\"nickName\": \"Ankit\"}";
+        this.mockMvc.perform(post("/user/create")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(request)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.nickName").value("Ankit"));
+
+    }
+
+    @Test
+    public void successfullyFindUserById() throws Exception {
+        this.mockMvc.perform(get("/user/search/1"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.nickName").value("Ankit"));
+    }
+}
